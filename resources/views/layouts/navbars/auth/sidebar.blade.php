af0f59f945a5b5244861970bb948c664e22f57fe
Yoshio <yoshio.sudewa@lidahbuayagroup.com>
2024-04-18 21:17:12 +0700

    crud user

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -79,7 +79,20 @@
                             class="fas fa-lg fa-list-ul ps-2 pe-2 text-center text-dark {{ Request::is('user-management') ? 'text-white' : 'text-dark' }} "
                             aria-hidden="true"></i>
                     </div>
-                    <span class="nav-link-text ms-1">User Management</span>
+                    <span class="nav-link-text ms-1">User Data</span>
+                </a>
+            </li>
+
+            <li class="nav-item pb-2">
+                <a class="nav-link {{ Request::is('member-management') ? 'active' : '' }}"
+                    href="{{ url('user-management') }}">
+                    <div
+                        class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i style="font-size: 1rem;"
+                            class="fas fa-lg fa-database ps-2 pe-2 text-center text-dark {{ Request::is('member-management') ? 'text-white' : 'text-dark' }} "
+                            aria-hidden="true"></i>
+                    </div>
+                    <span class="nav-link-text ms-1">Member Data</span>
                 </a>
             </li>
             {{-- <li class="nav-item mt-2">
